a307e5a07e2bcb55d1786083e662672d49b89699
maxlein <leingartnermax@gmail.com>
2020-05-04 09:11:35 +0000

    added update submodules call to install script

--- install_dotbot.sh
+++ install_dotbot.sh
@@ -3,5 +3,6 @@
 git init # initialize repository if needed
 git submodule add https://github.com/anishathalye/dotbot
 git config -f .gitmodules submodule.dotbot.ignore dirty # ignore dirty commits in the submodule
+git submodule update --init --recursive
 cp dotbot/tools/git-submodule/install .
 touch install.conf.yaml
